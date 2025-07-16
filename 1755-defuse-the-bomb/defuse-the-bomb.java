7adfa313ccc0305adbbc2ef2872e59ecda787797
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-07-16 08:37:55 -0500

    Time: 0 ms (100.00%) | Memory: 42.3 MB (53.18%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,27 +1,29 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int n = code.length;
         int[] result = new int[code.length];
 
-        if(k ==0){
+        int start = 1;
+        int end = k;
+
+        if(k == 0 ){
             return result;
         }
+        int sum = 0;
 
-        for(int i = 0; i < code.length; i++){
-            int sum = 0;
-            if(k > 0){
-                for(int j = 1; j <= k; j++){
-                    sum += code[(i+j) % n]; 
-                }
-            }
-            
-            if (k < 0){
-                for(int j = 1 ; j <= -k ; j++){
-                    sum += code[(i-j+n) % n];
-                }
-            }
-                            
-            result[i] = sum;
+        if(k < 0 ){
+            start = code.length - Math.abs(k);
+            end = code.length - 1;
+        }
+        for(int i = start; i <= end ; i++){
+            sum += code[i];
+        }
+
+        for(int i =0; i<code.length; i++){
+                result[i] = sum;
+                sum -= code[(start) % code.length];
+                sum += code[(end + 1 ) % code.length];
+                start++;
+                end++;
         }
 
         return result;
