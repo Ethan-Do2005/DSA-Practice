cb614bb697745ad0d58f61543dac9d84454f0143
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-08-12 21:46:12 -0500

    Time: 5 ms (7.14%) | Memory: 44.5 MB (61.58%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> list = new HashSet<>();
+        ListNode curr = head;
+
+        while(curr != null){
+            if(list.contains(curr)){
+                return true;
+            }
+            list.add(curr);
+            curr = curr.next;
+        }
+
+        return false;
+    }
+}
