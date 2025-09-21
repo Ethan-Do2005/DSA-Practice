e4b36a925d1ab29137565736f0e15bd2332efb51
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-09-21 17:06:32 -0400

    Time: 1 ms (13.62%) | Memory: 44.4 MB (28.02%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -1,13 +1,18 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int sign = 1; // We created if it postive it flip into -1
+        int n = nums.length;
+        int[] res = new int[n];
 
-        for(int num : nums){
-            if(num == 0 ) return 0;
-            else if( num < 0) sign = -sign; // flip -1 when freq is odd and flip into 1 when freq is even
+        res[0] = sgn(nums[0]);
+        if (res[0] == 0) return 0;
+
+        for (int i = 1; i < n; i++) {
+            int si = sgn(nums[i]);
+            if (si == 0) return 0;          // zero anywhere ⇒ sign 0
+            res[i] = res[i - 1] * si;       // stays in {-1, +1}
         }
-        return sign;
+        return res[n - 1];                   // already -1 or +1
     }
+
+    private int sgn(int x) { return x > 0 ? 1 : (x < 0 ? -1 : 0); }
 }
-
-
