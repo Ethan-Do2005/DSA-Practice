8079d4dfbd676d77f1e9baea5b98f3f7b91e7310
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-10-06 01:10:58 -0400

    Time: 7 ms (32.76%) | Memory: 43 MB (83.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Set<String> set = new HashSet<>();
-        if(s.length() != t.length()){
-            return false;
-        }
+        if(s.length() != t.length()) return false;
+        String s1 = s.toLowerCase();
+        String t1 = t.toLowerCase();
 
         int[] count = new int[26];
-        for(int i =0; i< s.length(); i++){
-            count[s.charAt(i) - 'a']++;
-        }
-        for(int i = 0; i<t.length(); i++){
-            count[t.charAt(i) - 'a']--;
+        for(int i = 0; i < s.length(); i++){
+            count[s1.charAt(i) - 'a']++;
+            count[t1.charAt(i) - 'a']--;
         }
 
-        for(int c : count){
-            if(c != 0){
-                return false;
-            }
+        for(int value : count){
+            if(value != 0) return false;
         }
-
         return true;
     }
 }
+/*
+counting logic
+*/
