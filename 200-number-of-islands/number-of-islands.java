10925ac36e45fda2b4189ace4626f624bfb7dc6e
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-10-08 12:45:49 -0400

    Time: 3 ms (84.64%) | Memory: 49.6 MB (54.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
-                    IsLand(grid, i,j);
+                    isLand(grid, i, j);
                     count++;
                 }
             }
@@ -14,29 +14,22 @@ class Solution {
         return count;
     }
 
-    private void IsLand(char[][] grid, int r, int c){
-        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] =='0'){
+    private void isLand(char[][] grid, int r, int c){
+        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0'){
             return;
         }
 
         grid[r][c] = '0';
 
-        IsLand(grid, r + 1, c);
-        IsLand(grid, r - 1, c);
-        IsLand(grid, r, c + 1);
-        IsLand(grid, r, c - 1);
+        isLand(grid, r + 1, c);
+        isLand(grid, r - 1, c);
+        isLand(grid, r, c + 1);
+        isLand(grid, r, c - 1);
     }
 }
 /*
 My idea:
-main method:
-nested loop to check satisfy of '1'
+using nested loop:
 
-helper method (private void method):
-base case 
-
-if condition (to check unsatify cases)
-
-go through 4 directions;
-
+Using helper method to check 4 directions
 */
