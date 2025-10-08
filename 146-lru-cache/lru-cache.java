1491451ce5d5352c18b7db8909eb58c6398571d8
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-10-08 12:40:02 -0400

    Time: 42 ms (91.87%) | Memory: 111.2 MB (97.37%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,23 +1,23 @@
 class LRUCache {
-    //Step 1: Node structure
-    private static class Node{
+    //Step 1: Node Structure
+    class Node{
         int key;
         int value;
+
         Node prev;
         Node next;
-
         Node(int k, int v){
             key = k;
             value = v;
         }
     }
 
+    //Step 3: variable outside
     private final int capacity;
-    private final Map<Integer, Node> map = new HashMap<>();
-    private final Node head = new Node(-1, -1);
-    private final Node tail = new Node(-1, -1);
-    
-    //Form this method in double linked list
+    private Map<Integer, Node> map = new HashMap<>();
+    private final Node head = new Node(-1,-1);
+    private final Node tail = new Node(-1,-1);
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
         head.next = tail;
@@ -26,66 +26,64 @@ class LRUCache {
     
     public int get(int key) {
         Node n = map.get(key);
+
         if(n == null) return -1;
         moveToHead(n);
         return n.value;
     }
-/*
-Get method:
-Using HashMap to check exist key or not
-Return key;
-Reutnr -1;
-*/
+    
     public void put(int key, int value) {
         Node n = map.get(key);
-        // 3 Steps:
-        // Step 1: check exits that key already?
+
+        //Exists key
         if(n != null){
             n.value = value;
             moveToHead(n);
             return;
         }
 
+        // Not exists
         Node node = new Node(key, value);
         map.put(key, node);
         addToHead(node);
 
+        //Check size
         if(map.size() > capacity){
             Node lru = removeTail();
             map.remove(lru.key);
-        }        
+        }
     }
-/*
-Put method:
-Using HashMap to add into with key and value 
-Based on capacity
-*/
 
-/*
-We also needd Double Linked List to make sure that which of pair key-value we will remove
+    //Step 2: Helper method
+    private void addToHead(Node n){
+        n.prev = head;
+        n.next = head.next;
+        head.next.prev = n;
+        head.next = n;
+    }
 
+    private void removeNode(Node n){
+        n.prev.next = n.next;
+        n.next.prev = n.prev;
+    }
+
+    private void moveToHead(Node n){
+        removeNode(n);
+        addToHead(n);
+    }
+
+    private Node removeTail(){
+        Node lru = tail.prev;
+        removeNode(lru);
+        return lru;
+    }
+}
+/*
+5 Steps:
+Step 1: Node Structure
+Step 2: Helper method 
+Step 3: 
 */
-// Helper method in Double LinkedList
-private void addToHead(Node n){
-    n.prev = head;
-    n.next = head.next;
-    head.next.prev = n;
-    head.next = n;
-}
-private void removeNode(Node n){
-    n.prev.next = n.next;
-    n.next.prev = n.prev;
-}
-private void moveToHead(Node n){
-    removeNode(n);
-    addToHead(n);
-}
-private Node removeTail(){
-    Node lru = tail.prev;
-    removeNode(lru);
-    return lru;
-}
-}
 
 /**
  * Your LRUCache object will be instantiated and called as such:
