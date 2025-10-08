2c0114e7de52b56f6b97f4b27c89c230fc9827d3
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-10-08 10:07:49 -0400

    Time: 0 ms (100.00%) | Memory: 44.8 MB (21.18%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -17,7 +17,7 @@ class Solution {
     public Node copyRandomList(Node head) {
         if(head == null) return null;
 
-        //Step 1:
+        //Step 1 
         Node curr = head;
         while(curr != null){
             Node copy = new Node(curr.val);
@@ -26,36 +26,38 @@ class Solution {
             curr = copy.next;
         }
 
-        //Step 2:
+        //Step 2
         curr = head;
         while(curr != null){
             if(curr.random != null){
-                curr.next.random = curr.random.next;
+                curr.next.random = curr.random.next; // A' -> C'
             }
+
             curr = curr.next.next;
         }
 
-        //Step 3
+        //Step 3:
         curr = head;
         Node dummy = new Node(0);
         Node node = dummy;
-
         while(curr != null){
             Node copy = curr.next;
             Node nextOri = copy.next;
 
             node.next = copy;
             node = copy;
-            
+
             curr.next = nextOri;
             curr = nextOri;
         }
 
-        return dummy.next;  
+        return dummy.next;
     }
 }
 /*
-Step 1: fake node
-Step 2: trace random node pointer
-Step 3: return into new Node
+3 step
+Step 1: Fake Node A -> A' -> B -> B'
+Step 2: poin fake random to another fake pointer
+Step 3: seperate A' into new ListNode
+
 */
