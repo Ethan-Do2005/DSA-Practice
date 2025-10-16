af8197592b0df6dd3814b3149be5f2a89f8031d4
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-10-15 22:36:06 -0500

    Time: 4 ms (93.86%) | Memory: 42.4 MB (98.16%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -3,24 +3,19 @@ class Solution {
         StringBuilder res = new StringBuilder();
         int i = s.length() - 1;
 
-        while(i >= 0 ){
-
-            //Remove space
-            while(i >= 0 && s.charAt(i) == ' ') i--;
+        while(i>=0){
+            
+            while(i >= 0 && s.charAt(i) ==' ') i--;
             if(i < 0) break;
 
-            // Mark
             int j = i;
+
             while(i >= 0 && s.charAt(i) != ' ') i--;
 
-            // Create a space for new res
             if(res.length() > 0) res.append(' ');
-
-            //Add string into new res
             res.append(s, i+1, j+1);
         }
 
         return res.toString();
-
     }
 }
