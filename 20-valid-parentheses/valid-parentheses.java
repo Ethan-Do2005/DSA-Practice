277733a90974567cc8463cedbc6d2b38ec69042c
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2025-12-28 22:39:59 -0600

    Time: 4 ms (42.35%) | Memory: 43 MB (81.70%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
+        
         Map<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put(']', '[');
