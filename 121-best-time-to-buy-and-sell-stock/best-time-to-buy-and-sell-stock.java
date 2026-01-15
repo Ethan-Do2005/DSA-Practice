00f0e8fefc7a05d4dfe4e6d98b91960c2ae52768
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-14 18:16:16 -0600

    Time: 2 ms (42.06%) | Memory: 94.5 MB (56.45%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,23 +1,54 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int i = 0; 
-        int j = 1;
-        int max = 0;
+        int buy = 0;
+        int sell = 1;
+        int result = 0;
 
-      while(j < prices.length){
-        if(prices[j] < prices[i]){
-            i=j;
-            continue;
+        while(sell < prices.length){
+            if(prices[sell] < prices[buy]){
+                buy = sell;
+            }
+
+            result = Math.max(result, prices[sell] - prices[buy]);
+            sell++;
         }
-        
-        max = Math.max(max, prices[j] - prices[i]);
-        j++;
-      }  
 
-      return max;
+        return result;
     }
 }
 /*
-sell > buy
-j > i 
+[7,1,5,3,6,4]
+0 <= 6
+
+7 <= 7 
+i++ => i = 1 ; j = 1
+
+
+
+
+output: 5 
+price 1 on day 2 and get the price 6 on day 5 and get subtract : 6 - 1 = 5 
+
+Contain negative numbers
+Yes
+[] 1 to 10^5 
+
+2 approach
+Approach A: brute force
+
+Approach B: 2 pointers
+int buy and sell
+sell > buy 
+
+[7,1,5,3,6,4]
+sell = 1;
+buy = 0;
+
+prices[sell]= 1
+prices[buy] = 7
+1 < 7 
+
+
+
+
 */
