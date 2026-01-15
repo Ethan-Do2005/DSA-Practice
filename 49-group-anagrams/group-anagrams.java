dc9a8bf70039465af944e7bd898d8ed7c7b5e854
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-14 18:27:57 -0600

    Time: 18 ms (27.16%) | Memory: 51.2 MB (6.23%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,18 +1,34 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> res = new HashMap<>();
+        Map<String, List<String>> result = new HashMap<>();
 
-        for(String s : strs){
+        for(String str : strs){
             int[] count = new int[26];
-            for(char c : s.toCharArray()){
+            for(char c : str.toCharArray()){
                 count[c - 'a']++;
             }
-            
+
             String key = Arrays.toString(count);
-            res.putIfAbsent(key, new ArrayList<>());
-            res.get(key).add(s);
+            result.putIfAbsent(key, new ArrayList<>());
+            result.get(key).add(str);
         }
 
-        return new ArrayList<>(res.values());
+        return new ArrayList<>(result.values());
     }
 }
+/*
+Range length
+1 --> 10^5
+Those will be upper or lowercase 
+=> lowercase
+
+Approach A
+
+Using HashMap
+ace cea
+Method
+[26]
+
+[1,0,1,0,1,0,0,0,]
+
+*/
