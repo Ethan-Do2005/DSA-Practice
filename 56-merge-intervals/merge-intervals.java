4433eae85405298784d7f9a074cee79f18f51d38
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-15 13:03:03 -0600

    Time: 8 ms (89.42%) | Memory: 49 MB (73.38%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,33 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
 
         List<int[]> result = new ArrayList<>();
 
-        int[] currInterval = intervals[0];
+        int[] currInterval = intervals[0]; 
         result.add(currInterval);
 
         for(int[] nextInterval : intervals){
-            int currEnd = currInterval[1];
-            int nextStart = nextInterval[0];
-            int nextEnd = nextInterval[1];
+            int startNext = nextInterval[0];
+            int endNext = nextInterval[1];
 
-            if(nextStart <= currEnd){
-                currInterval[1] = Math.max(currEnd, nextEnd);
+            int startCurr = currInterval[0];
+
+            if(currInterval[1] >= startNext){
+                currInterval[1] = Math.max(currInterval[1], endNext);
             }else{
                 currInterval = nextInterval;
                 result.add(currInterval);
             }
+
         }
 
         return result.toArray(new int[result.size()][]);
     }
 }
-/*
-Input:
-Interval= [[1,3], [3,6], [2,7], [8,9]]
-Output:
-Intervals= [[1,7], [8,9]]
-
-Idea:
-*/
