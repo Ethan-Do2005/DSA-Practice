24978822fa6f05d1ce605ffbff688e4ace71d233
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-16 15:42:03 -0600

    Time: 8 ms (89.51%) | Memory: 49.2 MB (28.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,27 +1,44 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> result = new ArrayList<>();
 
-        int[] currInterval = intervals[0]; 
-        result.add(currInterval);
+        int[] curInterval = intervals[0];
+        result.add(curInterval);
 
         for(int[] nextInterval : intervals){
             int startNext = nextInterval[0];
             int endNext = nextInterval[1];
 
-            int startCurr = currInterval[0];
-
-            if(currInterval[1] >= startNext){
-                currInterval[1] = Math.max(currInterval[1], endNext);
+            if(startNext <= curInterval[1]){
+                curInterval[1] = Math.max(curInterval[1], endNext);
             }else{
-                currInterval = nextInterval;
-                result.add(currInterval);
+                curInterval = nextInterval;
+                result.add(curInterval);
             }
-
         }
 
         return result.toArray(new int[result.size()][]);
     }
 }
+/*
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+
+1-->3
+  2----->6 
+             8-->10
+                      15--->18
+Output: [[1,6],[8,10],[15,18]]
+
+What is the range length of intervals
+1 <= intervals.length <= 10^4
+
+unsorted
+Apprach for this one Using ArrayList
+startCurrent, endCurrent
+startNext, endNext
+1-->6
+
+O(nlogn) time complexity and O(n) space complexity
+*/
