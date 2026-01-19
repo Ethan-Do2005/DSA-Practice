66a1639640f2e6e044d874aedcaba4effceeca83
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-19 14:02:17 -0600

    Time: 8 ms (89.48%) | Memory: 49.2 MB (29.61%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,44 +1,30 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
 
-        List<int[]> result = new ArrayList<>();
-
-        int[] curInterval = intervals[0];
-        result.add(curInterval);
+        List<int[]> list = new ArrayList<>();
+        
+        int[] currInterval = intervals[0];
+        list.add(currInterval);
 
         for(int[] nextInterval : intervals){
-            int startNext = nextInterval[0];
-            int endNext = nextInterval[1];
+            int nextStart = nextInterval[0];
+            int nextEnd = nextInterval[1];
 
-            if(startNext <= curInterval[1]){
-                curInterval[1] = Math.max(curInterval[1], endNext);
+            if(nextStart <= currInterval[1]){
+                currInterval[1] =  Math.max(nextEnd, currInterval[1]);
             }else{
-                curInterval = nextInterval;
-                result.add(curInterval);
+                currInterval = nextInterval;
+                list.add(currInterval);
             }
         }
 
-        return result.toArray(new int[result.size()][]);
+        return list.toArray(new int[list.size()][]);
     }
 }
 /*
-intervals = [[1,3],[2,6],[8,10],[15,18]]
-
-1-->3
-  2----->6 
-             8-->10
-                      15--->18
-Output: [[1,6],[8,10],[15,18]]
-
-What is the range length of intervals
-1 <= intervals.length <= 10^4
-
-unsorted
-Apprach for this one Using ArrayList
-startCurrent, endCurrent
-startNext, endNext
-1-->6
-
-O(nlogn) time complexity and O(n) space complexity
+1--->3
+   2---->6
+              8--->10
+                       15--->18
 */
