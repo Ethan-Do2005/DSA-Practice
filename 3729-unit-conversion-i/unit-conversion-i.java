18192cb604611381c87d920c5379b84d3773fd61
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-20 15:55:56 -0600

    Time: 39 ms (62.74%) | Memory: 229.9 MB (77.78%) - LeetSync

--- 3729-unit-conversion-i/unit-conversion-i.java
+++ 3729-unit-conversion-i/unit-conversion-i.java
@@ -1,49 +1,72 @@
 class Solution {
     int MOD = 1_000_000_007;
     List<int[]>[] graph;
-    int[] result;
-
+    long[] result;
     public int[] baseUnitConversions(int[][] conversions) {
-        int n = conversions.length + 1;
+        int maxNode = 0;
+        for(int[] conver : conversions){
+            maxNode = Math.max(maxNode, Math.max(conver[0], conver[1]));
+        }
+        int n = maxNode + 1;
 
         graph = new ArrayList[n];
-        //Create empty value in array list
         for(int i = 0; i < n; i++){
             graph[i] = new ArrayList<>();
         }
 
-        //Contains value into array list
         for(int[] conv : conversions){
             graph[conv[0]].add(new int[]{conv[1], conv[2]});
         }
 
-        result = new int[n];
+        result = new long[n];
+        Arrays.fill(result, -1);
+        
         result[0] = 1;
         dfs(0);
 
-        return result;
+        int[] finalResult = new int[n];
+        for(int i = 0; i < n; i++){
+            if(result[i] == -1){
+                finalResult[i] = 0;
+            }else{
+                finalResult[i] = (int) result[i];
+            }
+        }
+
+        return finalResult;
+
     }
-
+    
     private void dfs(int node){
-        for(int[] neightbor : graph[node]){
-            int next = neightbor[0];
-            int factor = neightbor[1];
+        for(int[] neighbor : graph[node]){
+            int nextNode = neighbor[0];
+            int factor = neighbor[1];
 
-            if(result[next] == 0 && next != 0){
-                result[next] = (int) ((long) result[node] * factor % MOD);
-                dfs(next);
+            if(result[nextNode] == -1){
+                result[nextNode] = (result[node] * factor) % MOD;
+                dfs(nextNode);
             }
         }
     }
 }
 /*
-Understanding concep it is kind of transparent money
-for example  [[0,1,2], [1,2,3]]
-Sign: [0] means Type 0, [1] means Type 1
-Always: [0] = 1
-for [0,1,2] => [1] = 2 x [0] = 2 x 1 = 2
-for [1,2,3] => [2] = 3 x [1] = 3 x 2 = 6
+[sourceUnit] = conversionFactor * [targetUnit]
+[0] = 1
 
-What they waant is to return each an array with its index is its Type
-For that example return =>[Type 0, Type 1, Type 2] = [1, 2, 6]
+[1] = 2 * [0] = 2;
+[2] = 3 * [0] = 3;
+[3] = 4 * [1] = 8;
+[4] = 5 * [1] = 10;
+[5] = 2 * [2] = 6;
+
+My approach: is using the weighted directed graph 
+3 Phase
+Phase 1: Size (based on maximum node)
+Phase 2: Building adjancent list 
+graph[0]: {[1,2]}
+graph[1]: {[2,3]}
+Phase 3: setting first node as 1 and going with dfs to traverse the parent to child
+Phase 4: return into
+
+Time and Space Complexity is O(V + E) where V is the nodes (units) and E is the edge case
 */
