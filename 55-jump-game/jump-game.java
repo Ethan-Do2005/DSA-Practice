ab19f66343b6ddc3585cadd934ee902701632065
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-22 10:34:41 -0600

    Time: 2 ms (85.00%) | Memory: 47.8 MB (70.79%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,13 +1,33 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int finalPos = nums.length - 1;
+        int finalPosition = nums.length -1;
 
-        for(int i = nums.length -2; i >=0; i--){
-            if(i + nums[i] >= finalPos){
-                finalPos = i;
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(i + nums[i] >= finalPosition){
+                finalPosition = i;
             }
         }
 
-        return finalPos==0;
+        return (finalPosition == 0);
     }
 }
+
+/*
+My approach is: 
+I will using greedy algorithms => get the best possible local and this will affect for global or the overall of the result
+Last index
+index   0 1 2 3 4
+nums = [2,3,1,1,4]
+last index = 3
+final position = 4
+
+3 + 1 = 4 
+finalpost = 3
+2 + 1 = 3
+finalpost = 2
+1 + 3 = 4 
+finalpost = 1
+if(index + nums[index] >= final position) => finalpost = i 
+=> to next inedx
+
+*/
