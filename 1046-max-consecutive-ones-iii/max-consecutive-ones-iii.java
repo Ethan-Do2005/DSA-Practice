bc5d1168a20d2948a65bab384344ad66c1838764
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-24 15:37:16 -0600

    Time: 4 ms (48.37%) | Memory: 52.1 MB (82.47%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,7 +3,7 @@ class Solution {
         int right = 0;
         int left = 0;
         int countZero = 0;
-        int maxLength = 0;
+        int maxLen = 0;
 
         while(right < nums.length){
             if(nums[right] == 0){
@@ -17,27 +17,16 @@ class Solution {
                 left++;
             }
 
-            maxLength = Math.max(maxLength, right - left + 1);
+            maxLen = Math.max(maxLen, right - left + 1);
             right++;
         }
 
-        return maxLength;
+        return maxLen;
     }
 }
 /*
 [1,1,1,0,0,0,1,1,1,1,0]
 
-[1,1,1,0,0,0]
-left =0
-right =0
-countZero = 3
-if(nums[right] == 0) => right++;
-while(countZero > k){
-    if(nums[left] == 0){
-        countZero--;
-    }
-    left++;
-}
+right and left
 
-maxLength= math.max(right-left + 1);
 */
