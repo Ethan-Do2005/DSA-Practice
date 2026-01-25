ada4f1c54869b594331e201219448604f7da1c16
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-25 15:26:23 -0600

    Time: 3 ms (6.16%) | Memory: 94.7 MB (23.25%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,56 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int sell = 0;
         int buy = 0;
-        int sell = 1;
-        int result = 0;
+        int maxProfit = 0;
 
-        while(sell < prices.length){
-            if(prices[sell] < prices[buy]){
+        while(sell < prices.length-1){
+            if(prices[buy] > prices[sell]){
                 buy = sell;
             }
-
-            result = Math.max(result, prices[sell] - prices[buy]);
             sell++;
+            maxProfit = Math.max(maxProfit, prices[sell] - prices[buy]);
         }
 
-        return result;
+        return maxProfit;
     }
 }
 /*
-
-O(n) time complexity and O(1) space complexity
-[7,1,5,3,6,4]
-0 <= 6
-
-7 <= 7 
-i++ => i = 1 ; j = 1
-
-
-
-
-output: 5 
-price 1 on day 2 and get the price 6 on day 5 and get subtract : 6 - 1 = 5 
-
-Contain negative numbers
-Yes
-[] 1 to 10^5 
-
-2 approach
-Approach A: brute force
-
-Approach B: 2 pointers
-int buy and sell
-sell > buy 
-
-[7,1,5,3,6,4]
-sell = 1;
-buy = 0;
-
-prices[sell]= 1
-prices[buy] = 7
-1 < 7 
-
-
-
-
+Using two pointer
+sell > buy => buy index is always small or equal to sell
 */
