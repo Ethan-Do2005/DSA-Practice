b13189d6f8e6ce7c7c6f20df1bad8091ed791e6f
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-25 16:42:13 -0600

    Time: 0 ms (100.00%) | Memory: 44.5 MB (89.30%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -2,45 +2,42 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int i = n - 2;
-        
-        //Step 1
+
         while(i >= 0 && nums[i] >= nums[i+1]){
             i--;
         }
 
-        //Step 2
         if(i >= 0){
             int j = n - 1;
-            while(nums[i] >= nums[j]){
+            while(nums[j] <= nums[i]){
                 j--;
             }
-            swap(nums,i,j);
+            swap(nums, i, j);
         }
+        
+        swapPart(nums, i + 1, n - 1);
 
-        swapDescending(nums, i+1, n-1);    
     }
 
     private void swap(int[] nums, int i, int j){
-        int temp;
-        temp = nums[i];
+        int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    private void swapDescending(int[] nums,int start,int end){
+    private void swapPart(int[] nums, int start, int end){
         while(start < end){
-            swap(nums, start, end);
+            swap(nums, start,end);
             start++;
             end--;
         }
     }
 }
 /*
-1.Finding the number outside number the descending part
-2. Find an bigger number than outside number in descending part to swap
-3. swap biggest and smallest number in descending part 
+[1,4,6,5,3,2]
 
- 0 1 2 3 4 5 6 7 8 9
-[1,5,2,4,7,6,5,4,2,1]
-
++Making pivot 
+is 4
++ Swap 4 with 5 
++ Swap descending part to ascending part 
 */
