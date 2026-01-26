56de9acc72136e3c9124c2c16fb3e99facc5b3de
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-25 22:30:54 -0600

    Time: 9 ms (39.70%) | Memory: 45.2 MB (95.87%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,13 +1,12 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-
-        Map<Integer, Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
             list.add(num);
-        }
+        } 
 
         Collections.sort(list, (a,b) -> {
             int freqA = map.get(a);
@@ -16,12 +15,13 @@ class Solution {
             if(freqA != freqB){
                 return freqA - freqB;
             }else{
-                return b - a;
+                return b - a; 
             }
         });
 
-        int[] result = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
+        int[] result = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++){
             result[i] = list.get(i);
         }
 
@@ -29,28 +29,5 @@ class Solution {
     }
 }
 /*
-what is the range length of nums
-from 1 to 100
-
-My approach for this using HashMap and List
-HashMap: count the freq num in nums
-List: sorted order
-
-nums = [1,1,2,2,2,3]
-HashMap{1:2, 2:3, 3:1}
-
-List {1,1,2,2,2,3}
-Collections.sort
-Same frequency of two index
-    => Compare by list value
-1 (freq 2)
-1 1 => 0 => it will order two
-1 2 => 3 - 2 => it still same 
-2 2 => 
-2 2 
-2 3 => 3 2
-list {1,1,2,2,3,2}
-2, 3 => 3 2 
-list {1,1,2,3,2,2}
-list {3,1,1,2,2,2}
+Using hashmap + list (collection comparation)
 */
