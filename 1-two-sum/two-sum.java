e010ae6bb0b391b8760ae16962ff801ff0c2e9e1
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-25 23:05:46 -0600

    Time: 3 ms (59.47%) | Memory: 47 MB (60.74%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,23 +3,14 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++){
-            int numFound = target - nums[i];
-
-            if(map.containsKey(numFound)){
-                return new int[]{map.get(numFound), i};
+            int targetNeeded = target - nums[i];
+            if(map.containsKey(targetNeeded)){
+                return new int[]{map.get(targetNeeded), i};
             }
+
             map.put(nums[i], i);
         }
-
+        
         return new int[]{};
     }
 }
-/*
-[2,7,11,15]
-First index: 2
-9 - 2 = 7
-Push 2 (i index) into map
-Second index:
-if it satisfy 
-=> i, get() 
-*/
