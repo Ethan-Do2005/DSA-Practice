d8c5bf003e88495464114f68995efb4a54cb7aa0
Ethan-Do2005 <130073859+Ethan-Do2005@users.noreply.github.com>
2026-01-30 21:22:50 -0500

    Time: 2 ms (99.13%) | Memory: 47.3 MB (17.37%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,14 +3,33 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++){
-            int targetNeeded = target - nums[i];
-            if(map.containsKey(targetNeeded)){
-                return new int[]{map.get(targetNeeded), i};
-            }
+            int num = nums[i];
+            int targetedNum = target - num;
 
-            map.put(nums[i], i);
+            if(map.containsKey(targetedNum)){
+                return new int[]{map.get(targetedNum), i};
+            }
+            map.put(num, i);
         }
-        
+
         return new int[]{};
     }
 }
+/*
+Constraints
+Range of this nums.length
+2 to 10^4
+Positive or negative number
+=> Both from 10^5 to 10^5
+
+My approach: HashMap and for loop
+[2,7,11,15]
+i = 0;
+Map{1: 2}
+9 - 2 = 7
+i = 1;
+Map{2: 1, 2: 7}
+9 - 7 = 2
+
+Reutn 0, 1
+*/
